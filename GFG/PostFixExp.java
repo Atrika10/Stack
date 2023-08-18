13bdb24ece493b7c30d07a4c91af0f58b65d2524
ATRIKA SHOW <readeratrika08@gmail.com>
2023-08-18 16:01:45 +0530

    added Approach of Evaluation of Postfix Expression

--- GFG/PostFixExp.java
+++ GFG/PostFixExp.java
@@ -1,6 +1,16 @@
 import java.util.*;
 public class PostFixExp {
 
+    /*
+     * Time & space complexity is  O(n)
+     * Approach
+     * Iterate over the given string, 
+     * step - 1 Each time, when I got a digit I'll push this in stack
+     * step -2 otherwise (when I got any operator) I'll pich 2 element from stack & perform operation (lastTop operator Firsttop). & push this value in the stack
+     * step - 3 return top element.
+     * 
+     */
+
     public static int evaluatePostFix(String s){
         Stack<Integer> st = new Stack<>();
         int val = 0;
@@ -10,12 +20,12 @@ public class PostFixExp {
                 int digit = ch -'0';
                 st.push(digit);   
             }else{
-                
+                // after getting an operator, I'll pick top 2 element
                 int e2 = st.pop();
                 int e1 = st.pop();
                 if(ch == '+'){
                     val =(int) e1+e2;
-                    st.push(val);
+                    st.push(val);  // pushed the value after performing operation
                 }else if(ch == '-'){
                     val =(int) e1-e2;
                     st.push(val);
@@ -29,7 +39,7 @@ public class PostFixExp {
                 }
             }
         }
-        return st.peek();
+        return st.peek(); // finally, return the top value
     }
     public static void main(String[] args) {
         String s = "231*+9-";
